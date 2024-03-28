528ec98fda68e333fc36834011fe17f2e6f17ee1
NathanLenas <nathanlenas@protonmail.com>
2024-03-28 14:37:19 +0100

    Modify db, add logs, update diagram

--- config/mariadb/tables/what2watch.sql
+++ config/mariadb/tables/what2watch.sql
@@ -17,15 +17,6 @@ CREATE TABLE user (
     FOREIGN KEY (group_id) REFERENCES user_group(group_id)
 );
 
-CREATE TABLE movie (
-    movie_id INT PRIMARY KEY,
-    title VARCHAR(255),
-    description TEXT,
-    year INT,
-    genre_id INT,
-    path_to_image VARCHAR(255),
-    FOREIGN KEY (genre_id) REFERENCES genre(genre_id)
-);
 
 CREATE TABLE user_group_membership (
     user_id INT,
@@ -45,10 +36,19 @@ CREATE TABLE user_genre_preferences (
 
 CREATE TABLE review (
     review_id INT PRIMARY KEY,
-    note VARCHAR(255),
+    rating INT,
     viewed BOOLEAN,
     user_id INT,
     movie_id INT,
     FOREIGN KEY (user_id) REFERENCES user(user_id),
     FOREIGN KEY (movie_id) REFERENCES movie(movie_id)
 );
+
+
+CREATE TABLE log {
+    log_id INT PRIMARY KEY,
+    log_level INT,
+    log_category VARCHAR(255),
+    log_time TIMESTAMP,
+    log_message TEXT,
+}
