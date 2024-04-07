5e92538d4e77ec60d265c084eb00e2ef8a4ab10e
NathanLenas <nathanlenas@protonmail.com>
2024-04-07 17:46:51 +0200

    feat(front): Nav bar + user nav

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,5 +1,19 @@
 <script lang="ts">
-    import { Button } from "$lib/components/ui/button";
+	import { Button } from '$lib/components/ui/button';
+	import { default as Nav } from '$lib/nav/header.svelte';
+    import { default as UserNav } from '$lib/nav/user-nav.svelte';
 </script>
-   
-<Button>Click me</Button>
+
+
+
+<div class="hidden flex-col md:flex">
+	<div class="border-b">
+		<div class="flex h-16 items-center px-4">
+			<Nav class="mx-6" />
+            <div class="ml-auto flex items-center space-x-4">
+				<!-- <Search /> -->
+				<UserNav />
+			</div>
+		</div>
+	</div>
+</div>
