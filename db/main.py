904981824db6606aebf454770e9b67aaa140ec16
garatquent <garatquent@eisti.eu>
2024-04-07 14:41:04 +0200

    CRUD of users in group

--- db/main.py
+++ db/main.py
@@ -18,18 +18,18 @@ app = FastAPI()
 # USERS
 # MOVIES
 
-@app.get("/users/{id}/movies", status_code=200)
-def get_seen_movies(id: int):
+@app.get("/users/{user_id}/movies", status_code=200)
+def get_seen_movies(user_id: int):
     
     return list_of_seen_movies
 
-@app.post("/users/{id}/movies", status_code=200)
-def add_movie_to_watched(id: int):
+@app.post("/users/{user_id}/movies", status_code=200)
+def add_movie_to_watched(user_id: int):
     
     return title+" is now watched"
 
-@app.delete("/users/{id}/movies", status_code=200)
-def remove_movie_from_watched(id: int):
+@app.delete("/users/{user_id}/movies", status_code=200)
+def remove_movie_from_watched(user_id: int):
     
     return title+" is now unwatched"
 
@@ -37,13 +37,13 @@ def remove_movie_from_watched(id: int):
 # USERS
 # RECOMMENDATIONS
 
-@app.get("/users/{id}/recommendations", status_code=200)
-def get_recommendations(id: int):
+@app.get("/users/{user_id}/recommendations", status_code=200)
+def get_recommendations(user_id: int):
     
     return list_of_recommendations
 
-@app.post("/users/{id}/recommendations", status_code=200)
-def add_recommendations(id: int):
+@app.post("/users/{user_id}/recommendations", status_code=200)
+def add_recommendations(user_id: int):
     
     return "Recommendations added"
 
@@ -51,33 +51,33 @@ def add_recommendations(id: int):
 # USERS
 # TASTES
 
-@app.get("/users/{id}/tastes", status_code=200)
-def get_tastes(id: int):
+@app.get("/users/{user_id}/tastes", status_code=200)
+def get_tastes(user_id: int):
     
     return list_of_tastes
 
 
 # USERS
 
-@app.get("/users/{id}", status_code=200)
-def get_user_infos(id: int):
-    c.execute("""SELECT * FROM user WHERE user_id LIKE %s""" % (id))
+@app.get("/users/{user_id}", status_code=200)
+def get_user_infos(user_id: int):
+    c.execute("""SELECT * FROM user WHERE user_id LIKE %s""" % (user_id))
     
     return c.fetchone()
 
-@app.post("/users/{id}", status_code=200)
-def add_user_infos(id: int, name: str, age: int):
+@app.post("/users/{user_id}", status_code=200)
+def add_user_infos(user_id: int, name: str, age: int):
     sql = "UPDATE user SET name = %s, age = %s WHERE user_id LIKE %s"    
-    values = (name, age, id)
+    values = (name, age, user_id)
     c.execute(sql, values)
     
     db.commit()
     
     return "User : "+name+" got updated"
 
-@app.delete("/users/{id}", status_code=200)
-def remove_user(id: int):
-    c.execute("""DELETE FROM user WHERE user_id LIKE %s""" % (id))
+@app.delete("/users/{user_id}", status_code=200)
+def remove_user(user_id: int):
+    c.execute("""DELETE FROM user WHERE user_id LIKE %s""" % (user_id))
     
     db.commit()
     
@@ -86,7 +86,7 @@ def remove_user(id: int):
 @app.post("/users", status_code=200)
 def create_user(name: str, age: int):
     sql = "INSERT INTO user (name, age) VALUES (%s, %s)"    
-    values = (name, 24)
+    values = (name, age)
     
     c.execute(sql, values)
     
@@ -108,9 +108,9 @@ def create_group(gp_name: str):
     
     return "Group : "+gp_name+" is now created"
 
-@app.get("/groups/{id}", status_code=200)
-def get_group_infos(id: int):
-    c.execute("""SELECT * FROM user_group WHERE group_id LIKE %s""" % (id))
+@app.get("/groups/{group_id}", status_code=200)
+def get_group_infos(group_id: int):
+    c.execute("""SELECT * FROM user_group WHERE group_id LIKE %s""" % (group_id))
     
     return c.fetchone()
 
@@ -118,16 +118,16 @@ def get_group_infos(id: int):
 # GROUPS
 # USERS
 
-@app.get("/groups/{id}/users", status_code=200)
-def get_group_users(id: int):
-    c.execute("""SELECT name FROM user, user_group_membership WHERE user_group_membership.group_id LIKE %s AND user.user_id = user_group_membership.user_id""" % (id))
+@app.get("/groups/{group_id}/users", status_code=200)
+def get_group_users(group_id: int):
+    c.execute("""SELECT name FROM user, user_group_membership WHERE user_group_membership.group_id LIKE %s AND user.user_id = user_group_membership.user_id""" % (group_id))
     
     return c.fetchall()
 
-@app.post("/groups/{id}/users", status_code=200)
-def add_user_to_group(id: int, user_id: int):
+@app.post("/groups/{group_id}/users/{user_id}", status_code=200)
+def add_user_to_group(group_id: int, user_id: int):
     sql = "INSERT INTO user_group_membership (user_id, group_id) VALUES (%s, %s)"    
-    values = (user_id, id)
+    values = (user_id, group_id)
     
     c.execute(sql, values)
     
@@ -135,10 +135,13 @@ def add_user_to_group(id: int, user_id: int):
     
     return "User added to group"
 
-@app.delete("/groups/{id}/users", status_code=200)
-def remove_user_from_group(id: int):
+@app.delete("/groups/{group_id}/users/{user_id}", status_code=200)
+def remove_user_from_group(group_id: int, user_id: int):
+    c.execute("""DELETE FROM user_group_membership WHERE user_id LIKE %s AND group_id LIKE %s""" % (user_id, group_id))
     
-    return username+" is now deleted from "+groupname
+    db.commit()
+    
+    return "User got deleted from its group"
 
 
 # GROUPS
