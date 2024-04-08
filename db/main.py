ae546f46173da5ac8480d7b19a67c2de4d786037
obito1903 <sam200133@gmail.com>
2024-04-08 21:05:15 +0200

    fix compose

--- db/main.py
+++ db/main.py
@@ -1,13 +1,15 @@
 from typing import Union
 from fastapi import FastAPI
+import os
 import MySQLdb
 
+
 db_config = {
-    'host': '127.0.0.1',
-    'port': 3306,
-    'user': 'moviefinder',
-    'passwd': 'moviefinder',
-    'db': 'moviefinder',
+    'host': os.environ['DB_HOST'] if 'DB_HOST' in os.environ else '127.0.0.1',
+    'port': int(os.environ['DB_PORT']) if 'DB_PORT' in os.environ else 3306,
+    'user': os.environ['DB_USER'] if 'DB_USER' in os.environ else 'moviefinder',
+    'passwd': os.environ['DB_PASS'] if 'DB_PASS' in os.environ else 'moviefinder',
+    'db': os.environ['DB_NAME'] if 'DB_NAME' in os.environ else 'moviefinder',
 }
 
 db = MySQLdb.connect(**db_config)
@@ -52,26 +54,26 @@ app = FastAPI(openapi_tags=tags_metadata)
 @app.get("/users/{user_id}/movies", status_code=200, tags=["Movies of Users"])
 def get_seen_movies(user_id: int):
     c.execute("""SELECT movie_id FROM review WHERE user_id LIKE %s""" % (user_id))
-    
+
     return c.fetchall()
 
 @app.post("/users/{user_id}/movies/{movie_id}", status_code=200, tags=["Movies of Users"])
 def add_movie_to_watched(user_id: int, movie_id: int, rating: int):
-    sql = "INSERT INTO review (rating, user_id, genre_id) VALUES (%s, %s, %s)"    
+    sql = "INSERT INTO review (rating, user_id, genre_id) VALUES (%s, %s, %s)"
     values = (rating, user_id, genre_id)
-    
+
     c.execute(sql, values)
-    
+
     db.commit()
-    
+
     return "User watched the movie"
 
 @app.delete("/users/{user_id}/movies/{movie_id}", status_code=200, tags=["Movies of Users"])
 def remove_movie_from_watched(user_id: int, movie_id: int):
     c.execute("""DELETE FROM review WHERE user_id LIKE %s AND movie_id LIKE %s""" % (user_id, movie_id))
-    
+
     db.commit()
-    
+
     return "User unwatched the movie"
 
 
@@ -81,18 +83,18 @@ def remove_movie_from_watched(user_id: int, movie_id: int):
 @app.get("/users/{user_id}/recommendations", status_code=200, tags=["Recommendations"])
 def get_recommendations(user_id: int):
     c.execute("""SELECT * FROM movie_recommendation WHERE user_id LIKE %s""" % (user_id))
-    
+
     return c.fetchall()
 
 @app.post("/users/{user_id}/recommendations", status_code=200, tags=["Recommendations"])
 def add_recommendations(user_id: int, movie_id: int, genre_id: int, accuracy: float):
-    sql = "INSERT INTO movie_recommendation (accuracy, group_id, user_id, movie_id) VALUES (%s, %s, %s, %s)"    
+    sql = "INSERT INTO movie_recommendation (accuracy, group_id, user_id, movie_id) VALUES (%s, %s, %s, %s)"
     values = (accuracy, group_id, user_id, movie_id)
-    
+
     c.execute(sql, values)
-    
+
     db.commit()
-    
+
     return "Recommendation added"
 
 
@@ -102,26 +104,26 @@ def add_recommendations(user_id: int, movie_id: int, genre_id: int, accuracy: fl
 @app.get("/users/{user_id}/tastes", status_code=200, tags=["Tastes of Users"])
 def get_tastes(user_id: int):
     c.execute("""SELECT genre_name FROM genre, user_genre_preferences WHERE user_genre_preferences.user_id LIKE %s AND genre.genre_id = user_genre_preferences.genre_id""" % (user_id))
-    
+
     return c.fetchall()
 
 @app.post("/users/{user_id}/tastes/{genre_id}", status_code=200, tags=["Tastes of Users"])
 def add_taste_for_user(user_id: int, genre_id: int):
-    sql = "INSERT INTO user_genre_preferences (user_id, genre_id) VALUES (%s, %s)"    
+    sql = "INSERT INTO user_genre_preferences (user_id, genre_id) VALUES (%s, %s)"
     values = (user_id, genre_id)
-    
+
     c.execute(sql, values)
-    
+
     db.commit()
-    
+
     return "Taste added to user"
 
 @app.delete("/users/{user_id}/tastes/{genre_id}", status_code=200, tags=["Tastes of Users"])
 def remove_taste_from_user(user_id: int, genre_id: int):
     c.execute("""DELETE FROM user_genre_preferences WHERE user_id LIKE %s AND genre_id LIKE %s""" % (user_id, genre_id))
-    
+
     db.commit()
-    
+
     return "Taste got deleted from user"
 
 # USERS
@@ -129,36 +131,36 @@ def remove_taste_from_user(user_id: int, genre_id: int):
 @app.get("/users/{user_id}", status_code=200, tags=["Users"])
 def get_user_infos(user_id: int):
     c.execute("""SELECT * FROM user WHERE user_id LIKE %s""" % (user_id))
-    
+
     return c.fetchone()
 
 @app.post("/users/{user_id}", status_code=200, tags=["Users"])
 def add_user_infos(user_id: int, name: str, age: int):
-    sql = "UPDATE user SET name = %s, age = %s WHERE user_id LIKE %s"    
+    sql = "UPDATE user SET name = %s, age = %s WHERE user_id LIKE %s"
     values = (name, age, user_id)
     c.execute(sql, values)
-    
+
     db.commit()
-    
+
     return "User : "+name+" got updated"
 
 @app.delete("/users/{user_id}", status_code=200, tags=["Users"])
 def remove_user(user_id: int):
     c.execute("""DELETE FROM user WHERE user_id LIKE %s""" % (user_id))
-    
+
     db.commit()
-    
+
     return "User got deleted"
 
 @app.post("/users", status_code=200, tags=["Users"])
 def create_user(name: str, age: int):
-    sql = "INSERT INTO user (name, age) VALUES (%s, %s)"    
+    sql = "INSERT INTO user (name, age) VALUES (%s, %s)"
     values = (name, age)
-    
+
     c.execute(sql, values)
-    
+
     db.commit()
-    
+
     return "User : "+name+" is now created"
 
 
@@ -166,19 +168,19 @@ def create_user(name: str, age: int):
 
 @app.post("/groups", status_code=200, tags=["Groups"])
 def create_group(gp_name: str):
-    sql = "INSERT INTO user_group (group_name) VALUES (%s)"    
+    sql = "INSERT INTO user_group (group_name) VALUES (%s)"
     values = [gp_name]
-    
+
     c.execute(sql, values)
-    
+
     db.commit()
-    
+
     return "Group : "+gp_name+" is now created"
 
 @app.get("/groups/{group_id}", status_code=200, tags=["Groups"])
 def get_group_infos(group_id: int):
     c.execute("""SELECT * FROM user_group WHERE group_id LIKE %s""" % (group_id))
-    
+
     return c.fetchone()
 
 
@@ -188,26 +190,26 @@ def get_group_infos(group_id: int):
 @app.get("/groups/{group_id}/users", status_code=200, tags=["Users from Groups"])
 def get_group_users(group_id: int):
     c.execute("""SELECT name FROM user, user_group_membership WHERE user_group_membership.group_id LIKE %s AND user.user_id = user_group_membership.user_id""" % (group_id))
-    
+
     return c.fetchall()
 
 @app.post("/groups/{group_id}/users/{user_id}", status_code=200, tags=["Users from Groups"])
 def add_user_to_group(group_id: int, user_id: int):
-    sql = "INSERT INTO user_group_membership (user_id, group_id) VALUES (%s, %s)"    
+    sql = "INSERT INTO user_group_membership (user_id, group_id) VALUES (%s, %s)"
     values = (user_id, group_id)
-    
+
     c.execute(sql, values)
-    
+
     db.commit()
-    
+
     return "User added to group"
 
 @app.delete("/groups/{group_id}/users/{user_id}", status_code=200, tags=["Users from Groups"])
 def remove_user_from_group(group_id: int, user_id: int):
     c.execute("""DELETE FROM user_group_membership WHERE user_id LIKE %s AND group_id LIKE %s""" % (user_id, group_id))
-    
+
     db.commit()
-    
+
     return "User got deleted from its group"
 
 
@@ -216,5 +218,5 @@ def remove_user_from_group(group_id: int, user_id: int):
 
 @app.get("/groups/{id}/tastes", status_code=200, tags=["Group tastes"])
 def get_group_tastes(id: int):
-    
+
     return list_of_group_tastes
