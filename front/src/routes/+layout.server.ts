724363317cc7cf282e9d6529c9a360aa286145c3
NathanLenas <nathanlenas@protonmail.com>
2024-04-11 14:55:56 +0200

    feat(front): Working auth

--- front/src/routes/+layout.server.ts
+++ front/src/routes/+layout.server.ts
@@ -1,11 +1,14 @@
-let sessionToken : string | null = null;
-let loggedIn = false;
-
-
-export function load({cookies}) {
-    const sessionCookie = cookies.get('authjs.session-token');
-
-    return {
-        sessionToken: sessionCookie
-    }
+import type { PageServerLoad } from "./$types"
+import { redirect } from "@sveltejs/kit"
+ 
+export const load: PageServerLoad = async (events) => {
+  const session = await events.locals.auth()
+ 
+//   if (!session?.user?.userId) {
+//     redirect(303, `/login`)
+//   }
+ 
+  return {
+    session,
+  }
 }
--- front/src/routes/+layout.svelte
+++ front/src/routes/+layout.svelte
@@ -3,9 +3,8 @@
 	import { ModeWatcher } from 'mode-watcher';
 	import { default as Header } from '$lib/nav/header.svelte';
 
-	export let data;
 </script>
 
-<Header sessionToken={data.sessionToken}/>
+<Header sessionToken={undefined}/>
 <ModeWatcher />
 <slot />
